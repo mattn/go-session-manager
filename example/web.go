7a5fc4356bf8eec97e96520ae717d95900ca055a
mattn <mattn.jp@gmail.com>
2011-02-15 21:15:27 +0900

    add example for web.go

---
+++ example/web.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"http/session"
+	"log"
+	"os"
+	"strings"
+	"template"
+	"web"
+)
+
+const page = `
+<!doctype html>
+<html>
+<meta charset="utf-8"/>
+<body>
+{.section session}
+{.section Value}
+Hi {@}.
+<form method="post" action="/logout">
+<input type="submit" name="method" value="logout" />
+</form>
+You will logout after 10 seconds. Then try to reload.
+{.or}
+<form method="post" action="/login">
+<label for="name">Name:</label>
+<input type="text" id="name" name="name" value="" />
+<input type="submit" name="method" value="login" />
+</form>
+{.end}
+{.end}
+</body>
+</html>
+`
+
+var fmap = template.FormatterMap{ "html": template.HTMLFormatter }
+var tmpl = template.MustParse(page, fmap)
+
+func main() {
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+	manager := session.NewSessionManager(logger)
+	manager.OnStart(func(session *session.Session) {
+		println("started new session")
+	})
+	manager.OnEnd(func(session *session.Session) {
+		println("abandon")
+	})
+	manager.SetTimeout(10)
+
+	GetSession := func(ctx *web.Context) *session.Session {
+		id, _ := ctx.GetSecureCookie("SessionId")
+		session := manager.GetSessionById(id)
+        ctx.SetSecureCookie("SessionId", session.Id, int64(manager.GetTimeout()))
+		ctx.SetHeader("Pragma", "no-cache", true)
+		return session
+	}
+
+    web.Config.CookieSecret = "7C19QRmwf3mHZ9CPAaPQ0hsWeufKd"
+	web.Get("/", func(ctx *web.Context) {
+		session := GetSession(ctx)
+		tmpl.Execute(ctx, map[string]interface{}{ "session": session })
+	})
+	web.Post("/login", func(ctx *web.Context) {
+		name := strings.Trim(ctx.Params["name"], " ")
+		if name != "" {
+			logger.Printf("User \"%s\" login", name)
+
+			// XXX: set user own object.
+			GetSession(ctx).Value = name
+		}
+		ctx.Redirect(302, "/")
+	})
+	web.Post("/logout", func(ctx *web.Context) {
+		session := GetSession(ctx)
+		if session.Value != nil {
+			// XXX: get user own object.
+			logger.Printf("User \"%s\" logout", session.Value.(string))
+			session.Abandon()
+		}
+		ctx.Redirect(302, "/")
+	})
+	web.Run(":6061")
+}
--- session.go
+++ session.go
@@ -62,6 +62,10 @@ func (session *Session) Abandon() {
 	}
 }
 
+func (session *Session) Cookie() string {
+	tm := time.SecondsToUTC(session.expire)
+	return fmt.Sprintf("SessionId=%s; path=/; expires=%s;", session.Id, tm.Format("Fri, 02-Jan-2006 15:04:05 -0700"))
+}
 
 type SessionManager struct {
 	sessionMap map[string]*Session
@@ -99,9 +103,7 @@ func (manager *SessionManager) OnEnd(f func(*Session)) { manager.onEnd = f }
 func (manager *SessionManager) SetTimeout(t uint) { manager.timeout = t }
 func (manager *SessionManager) GetTimeout() uint { return manager.timeout }
 
-func (manager *SessionManager) GetSession(res http.ResponseWriter, req *http.Request) *Session {
-	c := parseCookie(req)
-	id := c.Get("SessionId")
+func (manager *SessionManager) GetSessionById(id string) *Session {
 	if id == "" || !manager.Has(id) {
 		b := make([]byte, 32)
 		_, err := rand.Read(b)
@@ -114,9 +116,8 @@ func (manager *SessionManager) GetSession(res http.ResponseWriter, req *http.Req
 	}
 	tm := time.SecondsToUTC(time.LocalTime().Seconds() + int64(manager.timeout))
 	session, found := (*manager).sessionMap[id]
-	res.SetHeader("Set-Cookie", fmt.Sprintf("SessionId=%s; path=/; expires=%s;", id, tm.Format("Fri, 02-Jan-2006 15:04:05 -0700")))
 	if !found {
-		session = &Session{id, nil, tm.Seconds(), manager, res}
+		session = &Session{id, nil, tm.Seconds(), manager, nil}
 		(*manager).sessionMap[id] = session
 		f := (*manager).onStart
 		if f != nil {
@@ -126,47 +127,65 @@ func (manager *SessionManager) GetSession(res http.ResponseWriter, req *http.Req
 	return session
 }
 
+func (manager *SessionManager) GetSession(res http.ResponseWriter, req *http.Request) *Session {
+	c := parseCookie(req)
+	session := manager.GetSessionById(c.Get("SessionId"))
+	if res != nil {
+		session.res = res
+		res.SetHeader("Set-Cookie",
+			fmt.Sprintf("SessionId=%s; path=/; expires=%s;",
+			session.Id,
+			time.SecondsToUTC(session.expire).Format(
+				"Fri, 02-Jan-2006 15:04:05 -0700")))
+	}
+	return session
+}
+
 func (manager *SessionManager) Has(id string) bool {
 	_, found := (*manager).sessionMap[id]
 	return found
 }
 
-func parseCookie(req *http.Request) *Cookie {
-	h, found := req.Header["Cookie"]
-	if found && len(h) > 0 {
-		c := new(Cookie)
-		c.Items = make(map[string]string)
-		re, _ := regexp.Compile("[^=]+=[^;]+(; *(expires=[^;]+|path=[^;,]+|domain=[^;,]+|secure|HttpOnly))*,?")
-		rs := re.FindAllString(h, -1)
-		for _, ss := range rs {
-			m := strings.Split(ss, ";", -1)
-			for _, n := range m {
-				t := strings.Split(n, "=", 2)
-				if len(t) == 2 {
-					t[0] = strings.Trim(t[0], " ")
-					t[1] = strings.Trim(t[1], " ")
-					switch t[0] {
-					case "domain":
-						c.Domain = t[1]
-					case "path":
-						c.Path = t[1]
-					case "expires":
-						tm, err := time.Parse("Fri, 02-Jan-2006 15:04:05 MST", t[1])
-						if err != nil {
-							tm, err = time.Parse("Fri, 02-Jan-2006 15:04:05 -0700", t[1])
-						}
-						c.Expires = tm
-					case "secure":
-						c.Secure = true
-					case "HttpOnly":
-						c.HttpOnly = true
-					default:
-						c.Items[t[0]] = t[1]
+func StringToCookie(h string) *Cookie {
+	c := new(Cookie)
+	c.Items = make(map[string]string)
+	re, _ := regexp.Compile("[^=]+=[^;]+(; *(expires=[^;]+|path=[^;,]+|domain=[^;,]+|secure|HttpOnly))*,?")
+	rs := re.FindAllString(h, -1)
+	for _, ss := range rs {
+		m := strings.Split(ss, ";", -1)
+		for _, n := range m {
+			t := strings.Split(n, "=", 2)
+			if len(t) == 2 {
+				t[0] = strings.Trim(t[0], " ")
+				t[1] = strings.Trim(t[1], " ")
+				switch t[0] {
+				case "domain":
+					c.Domain = t[1]
+				case "path":
+					c.Path = t[1]
+				case "expires":
+					tm, err := time.Parse("Fri, 02-Jan-2006 15:04:05 MST", t[1])
+					if err != nil {
+						tm, err = time.Parse("Fri, 02-Jan-2006 15:04:05 -0700", t[1])
 					}
+					c.Expires = tm
+				case "secure":
+					c.Secure = true
+				case "HttpOnly":
+					c.HttpOnly = true
+				default:
+					c.Items[t[0]] = t[1]
 				}
 			}
 		}
-		return c
+	}
+	return c
+}
+
+func parseCookie(req *http.Request) *Cookie {
+	h, found := req.Header["Cookie"]
+	if found && len(h) > 0 {
+		return StringToCookie(h)
 	}
 	return nil
 }

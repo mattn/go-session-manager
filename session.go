1c1a6f4f0647ca108ebd939f27a826fb3295021e
Ben Cox <ben@playgen.com>
2013-11-21 10:39:01 +0000

    Set the default path to be "/"

--- session.go
+++ session.go
@@ -22,7 +22,7 @@ type SessionManager struct {
 	onStart    func(*Session)
 	onEnd      func(*Session)
 	timeout    uint
-	path	   string
+	path       string
 	mutex      sync.RWMutex
 }
 
@@ -45,6 +45,7 @@ func NewSessionManager(logger *log.Logger) *SessionManager {
 	manager := new(SessionManager)
 	manager.sessionMap = make(map[string]*Session)
 	manager.timeout = 300
+	manager.path = "/"
 	go func(manager *SessionManager) {
 		for {
 			l := time.Now().Unix()
